304b1314b8aeb94a22b34a12f34a0001c6c0ed47
johntranviact <john.tran@viact.ai>
2022-10-17 00:48:37 +0700

    chore: use babel webpack scss

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+const isProd = process.env.NODE_ENV === "production";
+
+const config = {
+  mode: isProd ? "production" : "development",
+  entry: {
+    index: "./src/index.tsx",
+  },
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "svg-url-loader",
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+};
+
+if (isProd) {
+  config.optimization = {
+    minimizer: [new TerserWebpackPlugin()],
+  };
+} else {
+  config.devServer = {
+    port: 9000,
+    open: true,
+    hot: true,
+    compress: true,
+    // stats: "errors-only",
+    // overlay: true,
+  };
+}
+
+module.exports = config;

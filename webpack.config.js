46af1fdc69c31aa774691967d9cfa3879b672a68
John Smith <rintran720@gmail.com>
2022-12-09 11:33:47 +0700

    feat: 99 performance score #8

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -62,6 +65,20 @@ const config = {
       favicon: './public/favicon.ico',
       inject: 'body',
     }),
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 8192,
+      minRatio: 0.8,
+    }),
+    new BrotliPlugin({
+      asset: '[path].br[query]',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+    // new BundleAnalyzerPlugin(),
   ],
 };
 
